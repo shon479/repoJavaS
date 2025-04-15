05616f9819b7b07d0f74bc3f950044b528b94010
Ruben Rolando Jumbo Domínguez <shon47new@gmail.com>
2025-04-15 11:30:24 -0500

    Creacion del archivo para calcular el imc

---
+++ IMC/calcularIMC.js
@@ -0,0 +1,25 @@
+function calcularIMC(peso, altura) {
+    const imc = peso / (altura * altura);
+    return imc;
+}
+
+const pesoUsuario= 80;
+const alturaUsuario = 1.55;
+const imcUsuario = calcularIMC(pesoUsuario,alturaUsuario);
+
+//imcUsuario.toFixed(2);
+
+if (imcUsuario < 18.5) {
+    console.log('Tu IMC se considera con un peso insuficiente: '+imcUsuario.toFixed(2));
+}else if (imcUsuario >= 18.5 && imcUsuario<=24.9) {
+        console.log('Tu IMC se considera saludable: '+imcUsuario.toFixed(2));
+    }else if (imcUsuario >= 25 && imcUsuario<=29.9) {
+            console.log('Tu IMC se considera sobrepeso: '+imcUsuario.toFixed(2));
+        }else if (imcUsuario >= 30 && imcUsuario<=39.9) {
+                console.log('Tu IMC se considera obesidad: '+imcUsuario.toFixed(2));
+            }else if (imcUsuario && 40) {
+                    console.log('Tu IMC se considera obesidad extrema: '+imcUsuario.toFixed(2));
+                }
+
+
+
